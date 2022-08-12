17b6387df02d59b358dbfd26bcbff63dd1fd91ed
twjasa <twjasa@gmail.com>
2022-08-12 02:58:08 -0400

    feat: implementation chess colors

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import "./App.css";
+import { range } from "./utils";
 const squareboard = Array.from({ length: 64 }, (v, i) => i + 1);
+const normalRows = [
+  ...range(1, 8),
+  ...range(17, 24),
+  ...range(33, 40),
+  ...range(49, 56),
+];
 function App() {
-  console.log(squareboard);
+  console.log(normalRows);
   return (
     <div className="App">
       <div className="Tablero">
         {squareboard.map((square) => {
-          let color = "";
-          if (square % 2 === 0) {
-            color = square % 8 == 0 && square != 0 ? "Brown" : "Ivory";
+          let condition;
+          if (normalRows.includes(square)) {
+            condition = square % 2 == 0;
           } else {
-            color = "Brown";
+            condition = square % 2 > 0;
           }
-          return <div className={`Square ${color}`} />;
+          return <div className={`Square ${condition ? "Brown" : "Ivory"}`} />;
         })}
       </div>
     </div>
