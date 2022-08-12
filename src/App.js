bbf4653902986e7410e72e839bd557db86581335
twjasa <twjasa@gmail.com>
2022-08-12 02:33:30 -0400

    feat: square colors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
-const squareboard = Array.from({length: 64}, (v, i) => i+1)
+import "./App.css";
+const squareboard = Array.from({ length: 64 }, (v, i) => i + 1);
 function App() {
-  console.log(squareboard)
+  console.log(squareboard);
   return (
     <div className="App">
-      <div className='Tablero'>
-        {
-          squareboard.map((square)=>  <div className={"Square"+square%2==0?"Brown":"Ivory"}></div>)
-        }                            
-                                   
+      <div className="Tablero">
+        {squareboard.map((square) => {
+          let color = "";
+          if (square % 2 === 0) {
+            color = square % 8 == 0 && square != 0 ? "Brown" : "Ivory";
+          } else {
+            color = "Brown";
+          }
+          return <div className={`Square ${color}`} />;
+        })}
       </div>
     </div>
   );
